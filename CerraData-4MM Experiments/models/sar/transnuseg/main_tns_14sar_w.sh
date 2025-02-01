508cdbf433d091d274828b4e766ac8c86fef2e53
Miranda, Mateus <104639136+ai4luc@users.noreply.github.com>
2025-02-01 13:36:10 -0300

    Add files via upload

---
+++ CerraData-4MM Experiments/models/sar/transnuseg/main_tns_14sar_w.sh
@@ -0,0 +1,15 @@
+export CUDA_LAUNCH_BLOCKING=1
+export PYTHON_PATH='/home/mateus/MateusPro/dlr_project/models/sar/transnuseg/train_14sar_w.py'
+export MODEL_TYPE='transnuseg'
+export ALPHA='0.3'
+export BETA='0.35'
+export GAMMA='0.35'
+export SHARING_RATIO='0.8'
+export NUM_EPOCH=100
+export LR=0.001
+export RANDOM_SEED=666
+export BATCH_SIZE=32
+export NUM_CHANNEL=2
+export NUM_CLASSES=14
+
+TORCH_USE_CUDA_DSA=1 python $PYTHON_PATH --model_type=$MODEL_TYPE --alpha=$ALPHA --beta=$BETA --gamma=$GAMMA --sharing_ratio=$SHARING_RATIO --lr=$LR --num_epoch=$NUM_EPOCH --random_seed=$RANDOM_SEED --batch_size=$BATCH_SIZE --num_channel=$NUM_CHANNEL --num_classes=$NUM_CLASSES
